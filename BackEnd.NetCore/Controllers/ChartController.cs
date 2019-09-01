e0bc3c84d78337ca56586e6d4034795a1184d18f
Mert Aksoy <36101620+mertcanaksoy@users.noreply.github.com>
2019-09-01 21:25:37 +0300

    Using C# with SignalR websockets
    Using C# with SignalR websockets

---
+++ BackEnd.NetCore/Controllers/ChartController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEnd.NetCore.DataStorage;
+using BackEnd.NetCore.HubConfig;
+using BackEnd.NetCore.TimerFeatures;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BackEnd.NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private IHubContext<ChartHub> _hub;
+        public ChartController(IHubContext<ChartHub> hub)
+        {
+            _hub = hub;
+        }
+        public IActionResult Get()
+        {
+            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferchartdata", DataManager.GetData()));
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
---
+++ BackEnd.NetCore/HubConfig/ChartHub.cs
@@ -0,0 +1,14 @@
+﻿using BackEnd.NetCore.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.NetCore.HubConfig
+{
+    public class ChartHub : Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data) => await Clients.All.SendAsync("broadcastchartdata", data);
+    }
+}
